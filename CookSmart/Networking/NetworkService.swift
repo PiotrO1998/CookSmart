45693f7c7532b3d56ce86bd750aff9fd140f8b23
Piotr <obara.piotrek@gmail.com>
2022-04-30 20:30:31 +0200

    Added endpoints

--- CookSmart/Networking/NetworkService.swift
+++ CookSmart/Networking/NetworkService.swift
@@ -8,4 +8,45 @@
 import Foundation
 import Alamofire
 
+struct NetworkService {
 
+    let baseUrl = "http://192.168.200.22/api/"
+
+    var headers: HTTPHeaders = []
+
+    init() {
+        let token = UserDefaults.standard.string(forKey: "token") ?? ""
+        
+        headers = [
+            "Content-Type": "application/json",
+            "Authorization": "Bearer \(token)"
+        ]
+    }
+    
+    enum Endpoints: String {
+        
+        // User
+        case registerUser = "register"
+        case signIn = "sanctum/token"
+        case signOut = "sign-out"
+        case getUserProfile = "user/profile"
+        case updateUserProfile = "user/update-profile"
+        case deleteUserProfile = "user/delete"
+        
+        // Recipe
+        case addRecipe = "recipe/store"
+        case editRecipe = "recipe/{recipe}/update"
+        case getCurrentUserRecipes = "recipes-all-user"
+        case getAllRecipes = "recipes-all"
+        case deleteRecipe = "recipe/{recipe}/delete"
+        
+        // Meal Plan
+        case addMealPlan = "meal-plans/store"
+        case editMealPlan = "mealplan/{mealplan/update"
+        case getCurrentUserMealPlans = "meal-plans-all-user"
+        case getAllMealPlans = "meal-plans-all"
+        case deleteMealPlan = "mealplan/{mealplan}/delete"
+        
+    }
+    
+}
