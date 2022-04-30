689e84553a584a0f887c416e90f017c27eb81a19
Piotr <obara.piotrek@gmail.com>
2022-04-30 17:56:24 +0200

    Added Sign In View Controller

---
+++ CookSmart/Controllers/SignInViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SignInViewController.swift
+//  CookSmart
+//
+//  Created by Piotr Obara on 30/04/2022.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func butttonSignInTapped(_ sender: UIButton) {
+        
+        
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ CookSmart/Networking/NetworkService.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkService.swift
+//  CookSmart
+//
+//  Created by Piotr Obara on 30/04/2022.
+//
+
+import Foundation
